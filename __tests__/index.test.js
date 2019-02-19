056078558b5866fc2aa44b55416736df4ba9e0f1
Roman Vr <romakf99@yandex.ru>
2019-02-19 17:18:18 +0300

    Complete two stage

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,8 +1,9 @@
 import path from 'path';
 import nock from 'nock';
 import os from 'os';
+import _ from 'lodash';
 import { promises as fs } from 'fs';
-import loadPage, { getNamePage } from '../src';
+import loadPage, { getNamePage, getNameAttr } from '../src';
 
 nock.disableNetConnect();
 
@@ -21,6 +22,23 @@ describe('Convert address to name', () => {
     const expected = 'yandex-ru';
 
     const fileName = getNamePage(address);
+    return expect(fileName).toBe(expected);
+  });
+});
+
+describe('Converted local addres to name', () => {
+  it('set 1: /courses', () => {
+    const address = '/courses';
+    const expected = 'courses';
+
+    const fileName = getNameAttr(address);
+    return expect(fileName).toBe(expected);
+  });
+  it('set 2: /cgn-ccc/image.rt.img', () => {
+    const address = '/cgn-ccc/image.rt.img';
+    const expected = 'cgn-ccc-image-rt.img';
+
+    const fileName = getNameAttr(address);
     return expect(fileName).toBe(expected);
   });
 });
@@ -47,4 +65,46 @@ test('Download page', async () => {
   const actualFile = await fs.readFile(outputFile);
 
   expect(actualFile.toString()).toBe(expectedFile.toString());
+});
+
+const tagsResource = {
+  link: { url: '/local/courses', pathLocal: 'local/courses.html', contentType: 'text/html' },
+  script: { url: '/local/script.js', pathLocal: 'local/script.js', contentType: 'text/plain' },
+  img: { url: '/local/cats.jpg', pathLocal: 'local/cats.jpg', contentType: 'image/jpg' },
+};
+
+beforeAll(() => {
+  const testLocalFile = 'localRes.html';
+  const domain = 'https://local.ru';
+  const localePagePath = path.join(pathTemplate, testLocalFile);
+  nock(domain)
+    .get('/')
+    .replyWithFile(
+      200,
+      localePagePath,
+      { 'Content-Type': 'text/html' },
+    );
+  _.keys(tagsResource).forEach((key) => {
+    const { url, pathLocal, contentType } = tagsResource[key];
+    const filePath = path.join(pathTemplate, pathLocal);
+    nock(domain)
+      .get(url)
+      .replyWithFile(
+        200,
+        filePath,
+        { 'Content-Type': contentType },
+      );
+  });
+});
+
+test('Download page with resources', async () => {
+  const addressTest = 'https://local.ru';
+  const pageFileName = 'local-ru.html';
+
+  await loadPage(addressTest, os.tmpdir());
+
+  const actualPageFile = await fs.readFile(path.join(os.tmpdir(), pageFileName));
+  const expectedPageFile = await fs.readFile(path.join(pathTemplate, pageFileName));
+
+  expect(actualPageFile.toString()).toBe(expectedPageFile.toString());
 });
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import url from 'url';
 import _ from 'lodash';
 import path from 'path';
+import cheerio from 'cheerio';
 import { promises as fs } from 'fs';
 
 export const getNamePage = (address) => {
@@ -9,11 +10,64 @@ export const getNamePage = (address) => {
   return `${urlObj.host}${_.trimEnd(urlObj.pathname, '/')}`.replace(/\W/g, '-');
 };
 
+export const getNameAttr = (address) => {
+  const urlObj = url.parse(address);
+  urlObj.path = _.trimStart(urlObj.path, '/');
+  const ext = path.extname(urlObj.path);
+  urlObj.path = urlObj.path.substring(0, urlObj.path.length - ext.length);
+  const convertedNameAttr = `${urlObj.path.replace(/\W/g, '-')}${ext}`;
+  return convertedNameAttr;
+};
+
+const typesLocalResources = {
+  link: { attr: 'href' },
+  script: { attr: 'src' },
+  img: { attr: 'src' },
+};
+
+const getSelector = tag => `${tag}[${typesLocalResources[tag].attr}^=\\/]`;
+
+const loadArraybufferResource = (address, output) => axios
+  .get(address, { responseType: 'arraybuffer' })
+  .then(response => fs.writeFile(output, response.data));
+
+const downloadResourcesByTag = (tag, dom, dirLocal, address, output) => {
+  const selector = getSelector(tag);
+  const resources = dom(selector).map((index, elem) => {
+    const attrValue = dom(elem).attr(typesLocalResources[tag].attr);// получили значение
+    const newNameAttr = getNameAttr(attrValue);// новое значение аттрибута
+    const fullPathAttr = path.join(output, dirLocal, newNameAttr);// путь сохраннеия ресурса на диск
+    const addressAttr = url.resolve(address, attrValue);// url attr для загрузки
+    dom(elem).attr(typesLocalResources[tag].attr, path.join(dirLocal, newNameAttr));
+    return loadArraybufferResource(addressAttr, fullPathAttr);// Промис с загрузкой ресурса
+  }).get();
+  return resources;
+};
+
+const downloadLocalResources = (html, address, output) => {
+  const dom = cheerio.load(html, {
+    decodeEntities: false,
+  });
+  const dirLocal = `${getNamePage(address)}_files`;
+  const pathDirLocal = path.join(output, dirLocal);
+  return fs.mkdir(pathDirLocal)
+    .then(() => {
+      const localResources = _.keys(typesLocalResources)
+        .reduce(
+          (acc, tag) => [...acc, ...downloadResourcesByTag(tag, dom, dirLocal, address, output)],
+          [],
+        );
+      return Promise.all(localResources);
+    })
+    .then(() => `${dom.html()}\n`);
+};
+
 const loadPage = (address, output) => {
   const fileName = path.join(output, `${getNamePage(address)}.html`);
   return axios.get(address)
     .then(response => response.data)
-    .then(html => fs.writeFile(fileName, html));
+    .then(html => downloadLocalResources(html, address, output))
+    .then(newHtml => fs.writeFile(fileName, newHtml));
 };
 
 export default loadPage;
